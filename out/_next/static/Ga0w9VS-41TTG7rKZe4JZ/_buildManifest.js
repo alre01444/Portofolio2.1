b4317bc48fd28119f0174b57390efe645499e643
Rayyan <ahmadalrayyann@gmail.com>
2026-08-05 19:09:36 +0700

    Add files via upload

---
+++ out/_next/static/Ga0w9VS-41TTG7rKZe4JZ/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Portofolio2.1//_next/:path+",
+        "destination": "/Portofolio2.1/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
